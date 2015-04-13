9b18c4b5f38f4e3a3f1205f02a40bb3de68567c1
Cameron Newland <cameron@reasonmedia.com>
2015-04-12 22:07:24 -0700

    Got more of homepage done

--- home.php
+++ home.php
@@ -1,21 +1,65 @@
 <div class="container">
-	<div class="posts-container">
-		<article class="post">
-			<header>
-				<h3>Post Title</h3>
-				<div class="username-block">
-					<span class="username">13</span>
-				</div>
-				<div class="hype-count">
-					<span class="count">13</span>
-				</div>
-			</header>
-			<figure>
-				<img src="img/post-placeholder.jpg">
-			</figure>
-			<footer>
-				<div class="post-meta">Post Meta</div>
-			</footer>
-		</article>
+	<div class="row">
+		<div class="column small-8">
+			<section class="posts-container">
+				<article class="post">
+					<header>
+						<h3>
+							<a href="posts/post-id">Post Title
+							</a>
+						</h3>
+						<div class="username-block">
+							<span class="username">Username</span>
+						</div>
+						<div class="hype-count">
+							<span class="count">13</span>
+						</div>
+					</header>
+					<figure class="post-image">
+						<img src="img/post-placeholder.jpg">
+					</figure>
+					<footer>
+						<div class="post-meta">Post Meta</div>
+					</footer>
+				</article>
+				<article class="post">
+					<header>
+						<h3>Post Title</h3>
+						<div class="username-block">
+							<span class="username">Username</span>
+						</div>
+						<div class="hype-count">
+							<span class="count">13</span>
+						</div>
+					</header>
+					<figure class="post-image">
+						<img src="img/post-placeholder.jpg">
+					</figure>
+					<footer>
+						<div class="post-meta">Post Meta</div>
+					</footer>
+				</article>
+				<article class="post">
+					<header>
+						<h3>Post Title</h3>
+						<div class="username-block">
+							<span class="username">Username</span>
+						</div>
+						<div class="hype-count">
+							<span class="count">13</span>
+						</div>
+					</header>
+					<figure class="post-image">
+						<img src="img/post-placeholder.jpg">
+					</figure>
+					<footer>
+						<div class="post-meta">Post Meta</div>
+					</footer>
+				</article>
+			</section>
+		</div>
+		<div class="column small-4">
+			<h3>Sidebar</h3>
+		</div>
 	</div>
 </div>
